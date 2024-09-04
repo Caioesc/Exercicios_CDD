d65a726ed0ac109d053a3d0eb2e2eec60d5908a5
CaioEsc <caioheraclio@gmail.com>
2024-09-04 14:04:44 -0300

    Exercicios_CDD

--- exercicio09.py
+++ exercicio09.py
@@ -1,10 +1,13 @@
 litros = float(input("Informe quantos litros foram abastecidos: "))
-comb = input("Informe o tipo de combustível (Digite E para etanol e G para gasolina: ")
+comb = input("Informe o tipo de combustível (Digite E para etanol e G para gasolina): ")
 preco = 0
 
-if comb == "E":
+if comb == "E" or comb == "e":
     preco = litros * 4.90
-    print(f"O valor total foi de R${preco}")
+    print(f"O valor total foi de R${preco: .2f}")
 else:
-    preco = litros * 5.80
-    print(f"O valor total foi de R${preco}")
+    if comb == "G" or comb == "g":
+        preco = litros * 5.80
+        print(f"O valor total foi de R${preco: .2f}")
+    else:
+        print("Tipo de combustível inválido!")
