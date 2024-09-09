0d62441608b0775435dfbeeee351646e9dceffcc
Caioesc <caioheraclio@gmail.com>
2024-09-09 15:37:03 -0300

    Exercicios_CDD

---
+++ exercicio15.py
@@ -0,0 +1,8 @@
+"""Ler uma variável de número inteiro e mostrar a tabuada de multiplicação desse número (1-10)
+   Formato da saída: 1 x 5 = 5; 2 x 5 = 10; etc..."""
+
+numero = int(input("Digite o número desejado: "))
+for i in range (1, 11):
+    tabuada = i * numero
+    print(f"{i} x {numero} = {tabuada}")
+    i += 1
