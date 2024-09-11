066c496687b4d09f6c641dd0e932b27d47a9d3c5
Caioesc <caioheraclio@gmail.com>
2024-09-11 16:58:48 -0300

    Exercicios_CDD_dia_11092024

--- exercicio24.py
+++ exercicio24.py
@@ -1,9 +1,14 @@
 """Ler um valor N e imprimir todos os valores inteiros entre 1 (inclusive) e N (inclusive).
    Considere que o N será sempre maior que ZERO."""
 
-numero = int(input("Informe um número: "))
-cont = 1
+repetir = 1
 
-while cont <= numero:
-    print(cont)
-    cont += 1
+while repetir != 2:
+    numero = int(input("Informe um número: "))
+    cont = 1
+    repetir = 1
+    while cont <= numero:
+        print(cont)
+        cont += 1
+    repetir = int(input("\nDeseja fazer novamente? \n 1 - Sim \n 2 - Não"))
+print("Operação encerrada!")
